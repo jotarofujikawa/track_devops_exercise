99dca2ff0458f6d281ec33f4471f68994c849102
jotarofujikawa <jotaro.fujikawa@tcs.com>
2026-06-12 11:55:03 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 def add(a, b, c=0):
-  try:
-    return int(a + b) + int(c)
-  except:
-    return "error"
+  if not all(isinstance(x, (int, float)) for x in [a, b, c]):
+    return -1
+  if not all(0 <= x <= 10 for x in [a, b, c]):
+    return -2
+  return int(a + b) + int(c)
